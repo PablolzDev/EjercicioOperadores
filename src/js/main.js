d637a902e8722dd11f966822ffcbc3ce2251f29f
PablolzDev <pabloinprogramacion@gmail.com>
2024-04-18 13:28:54 -0500

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,150 @@
+// Exercise 1
+console.groupCollapsed("Exercise 1")
+    console.log(35>25)
+console.groupEnd()
+
+// Exercise 2
+console.groupCollapsed("Exercise 2")
+    console.log(45<30)
+console.groupEnd()
+
+// Exercise 3
+console.groupCollapsed("Exercise 3")
+    console.log(50=="50")
+console.groupEnd()
+
+// Exercise 4
+console.groupCollapsed("Exercise 4")
+    console.log("40" === 40)
+console.groupEnd()
+
+// Exercise 5
+console.groupCollapsed("Exercise 5")
+    console.log("apple" != "orange" )
+console.groupEnd()
+
+// Exercise 6
+console.groupCollapsed("Exercise 6")
+    console.log("apple" !== "Apple")
+console.groupEnd()
+
+// Exercise 7
+console.groupCollapsed("Exercise 7")
+    let variable;
+    console.log(variable)
+console.groupEnd()
+
+// Exercise 8
+console.groupCollapsed("Exercise 8")
+    console.log(typeof variable)
+console.groupEnd()
+
+// Exercise 9
+console.groupCollapsed("Exercise 9")
+console.log(null == undefined)
+console.groupEnd()
+
+// Exercise 10
+console.groupCollapsed("Exercise 10")
+    console.log(null === undefined)
+console.groupEnd()
+
+// Exercise 11
+console.groupCollapsed("Exercise 11")
+    console.log("2.75" > 2.5);
+console.groupEnd()
+
+// Exercise 12
+console.groupCollapsed("Exercise 12")
+    console.log("2.75" > 2.75)
+console.groupEnd()
+
+// Exercise 13
+console.groupCollapsed("Exercise 13")
+    console.log("2.75" >= 2.75)
+console.groupEnd()
+
+// Exercise 14
+console.groupCollapsed("Exercise 14")
+    console.log("2.75" <= 2.75)
+console.groupEnd()
+
+// Exercise 15
+console.groupCollapsed("Exercise 15")
+    console.log("2.75" > 2.8)
+console.groupEnd()
+
+// Exercise 16
+console.groupCollapsed("Exercise 16")
+console.log("2.75" > 2.7)
+console.groupEnd()
+
+// Exercise 17
+console.groupCollapsed("Exercise 17")
+    console.log("John" != "jhon")
+console.groupEnd()
+
+// Exercise 18
+console.groupCollapsed("Exercise 18")
+    console.log("John" != "Jon") 
+console.groupEnd()
+
+// Exercise 19
+console.groupCollapsed("Exercise 19")
+    console.log(30 !== "30")
+console.groupEnd()
+
+// Exercise 20
+console.groupCollapsed("Exercise 20")
+    console.log(25>>2)
+console.groupEnd()
+
+// Exercise 21
+console.groupCollapsed("Exercise 21")
+    console.log(3<<8);
+console.groupEnd()
+
+// Exercise 22
+console.groupCollapsed("Exercise 22")
+    console.log(15 == 15)
+console.groupEnd()
+
+// Exercise 23
+console.groupCollapsed("Exercise 23")
+    console.log(20 != 30)
+console.groupEnd()
+
+// Exercise 24
+console.groupCollapsed("Exercise 24")
+    console.log("15" > 30)
+console.groupEnd()
+
+// Exercise 25
+console.groupCollapsed("Exercise 25")
+    console.log(20 < 40)
+console.groupEnd()
+
+// Exercise 26
+console.groupCollapsed("Exercise 26")
+    console.log(100 >= "99")
+console.groupEnd()
+
+// Exercise 27
+console.groupCollapsed("Exercise 27")
+    console.log(500 === "500")
+console.groupEnd()
+
+// Exercise 28
+console.groupCollapsed("Exercise 28")
+    console.log(750 === 750)
+console.groupEnd()
+
+// Exercise 29
+console.groupCollapsed("Exercise 29")
+    console.log(5 !== 5)
+console.groupEnd()
+
+// Exercise 30
+console.groupCollapsed("Exercise 30")
+    console.log(3 >5 && 4 < 6)
+console.groupEnd()
